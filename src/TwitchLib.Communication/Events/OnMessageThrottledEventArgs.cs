78d9347e2237b4f2dbe15e40f27a8f861b6161ce
CMR <github.christian-riedel@outlook.de>
2023-03-08 20:32:41 +0100

    ToString()

--- src/TwitchLib.Communication/Events/OnMessageThrottledEventArgs.cs
+++ src/TwitchLib.Communication/Events/OnMessageThrottledEventArgs.cs
@@ -21,5 +21,9 @@ namespace TwitchLib.Communication.Events
         {
             OnThrottledEventArgs = onThrottledEventArgs;
         }
+        public override string ToString()
+        {
+            return OnThrottledEventArgs.ToString();
+        }
     }
 }
--- src/TwitchLib.Communication/Events/OnThrottledEventArgs.cs
+++ src/TwitchLib.Communication/Events/OnThrottledEventArgs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TwitchLib.Communication.Events
 {
@@ -12,5 +13,15 @@ namespace TwitchLib.Communication.Events
         public long SentCount { get; set; }
         public TimeSpan Period { get; set; }
         public uint AllowedInPeriod { get; set; }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"{nameof(Reason)}: {Reason}");
+            builder.AppendLine($"{nameof(ItemNotSent)}: {ItemNotSent}");
+            builder.AppendLine($"{nameof(SentCount)}: {SentCount}");
+            builder.AppendLine($"{nameof(Period)}: {Period}");
+            builder.AppendLine($"{nameof(AllowedInPeriod)}: {AllowedInPeriod}");
+            return builder.ToString();
+        }
     }
 }

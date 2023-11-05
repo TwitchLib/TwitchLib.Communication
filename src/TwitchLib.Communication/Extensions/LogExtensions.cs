4f178458ef89cb6a303fbbef6064ed7b10b4655b
AoshiW <ondrula98@gmail.com>
2023-11-05 12:05:12 +0100

    net 8 changes

--- src/TwitchLib.Communication/Extensions/LogExtensions.cs
+++ src/TwitchLib.Communication/Extensions/LogExtensions.cs
@@ -1,5 +1,4 @@
-﻿#pragma warning disable SYSLIB1006 // Multiple logging methods cannot use the same event id within a class
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Logging;
 
 namespace TwitchLib.Communication.Extensions
@@ -17,16 +16,16 @@ namespace TwitchLib.Communication.Extensions
             TraceMethodCallCore(logger, type, callerMemberName, callerLineNumber);
         }
 
-        [LoggerMessage(0, LogLevel.Trace, "{type}.{callerMemberName} at line {callerLineNumber} is called")]
+        [LoggerMessage(LogLevel.Trace, "{type}.{callerMemberName} at line {callerLineNumber} is called")]
         static partial void TraceMethodCallCore(this ILogger logger, Type type, string callerMemberName, int callerLineNumber);
 
-        [LoggerMessage(0, LogLevel.Error, "Exception in {type}.{callerMemberName} at line {callerLineNumber}")]
+        [LoggerMessage(LogLevel.Error, "Exception in {type}.{callerMemberName} at line {callerLineNumber}")]
         public static partial void LogExceptionAsError(this ILogger logger, Type type, Exception exception, [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = 0);
 
-        [LoggerMessage(0, LogLevel.Information, "Exception in {type}.{callerMemberName} at line {callerLineNumber}")]
+        [LoggerMessage(LogLevel.Information, "Exception in {type}.{callerMemberName} at line {callerLineNumber}")]
         public static partial void LogExceptionAsInformation(this ILogger logger, Type type, Exception exception, [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = 0);
 
-        [LoggerMessage(0, LogLevel.Trace, "{type}.{callerMemberName} at line {callerLineNumber}: {action}")]
+        [LoggerMessage(LogLevel.Trace, "{type}.{callerMemberName} at line {callerLineNumber}: {action}")]
         public static partial void TraceAction(this ILogger logger, Type type, string action, [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = 0);
     }
 }
